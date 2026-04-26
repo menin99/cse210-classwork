1d224588065f4b9a1d3403a4848054c8d9403550
menin99 <tyleraverysmith2007@gmail.com>
2026-04-25 21:00:59 -0600

    Did assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percent did you get as a grade?");
+        int gradePercent = int.Parse(Console.ReadLine());
+        string letter = "";
+        if (gradePercent >= 90) {
+            letter = "A";
+        }
+        else if (gradePercent >= 80) {
+            letter = "B";
+        }
+        else if (gradePercent >= 70) {
+            letter = "C";
+        }
+        else if (gradePercent >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+        Console.WriteLine("Your letter grade is " + letter + ".");
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You passed.");
+        } else
+        {
+            Console.WriteLine("You did not pass.");
+        }
     }
 }
