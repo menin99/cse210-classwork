98ee4def3db5fddb091a19fec12b0cf41e88823a
menin99 <tyleraverysmith2007@gmail.com>
2026-05-02 13:57:37 -0600

    Finished Number Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int number = rnd.Next(1, 101);
+        Console.WriteLine("A random number has been generated from 1 to 100.\nIt is your job to guess it\nWhat is your guess?");
+        int guess = int.Parse(Console.ReadLine());
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            } else
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Good Job, You Guessed It.");
     }
 }
