bc849cc04edddc477140c81d33fe78bb581ecb5a
menin99 <tyleraverysmith2007@gmail.com>
2026-05-18 12:00:45 -0600

    finished prepare

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+        Random random = new Random();
+        Fraction randomFraction = new Fraction();
+        for (int i = 1; i <= 20; i++)
+        {
+            int top = random.Next(1, 11);
+            int bottom = random.Next(1, 11);
+            randomFraction.SetTop(top);
+            randomFraction.SetBottom(bottom);
+            Console.WriteLine($"Fraction {i}: String: {randomFraction.GetFractionString()} Number is: {randomFraction.GetDecimalValue()}");
+        }
     }
 }
