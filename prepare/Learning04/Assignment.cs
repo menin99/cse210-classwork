f7c1d7ef8dd16004f7de6ed0361a3877142d6f10
menin99 <tyleraverysmith2007@gmail.com>
2026-06-01 11:59:53 -0600

    Finished prepare

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _topic;
+    private string _studentName;
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+    public string GetTopic()
+    {
+        return _topic;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("John Doe", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        MathAssignment a2 = new MathAssignment("Arthur Smith", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        WritingAssignment a3 = new WritingAssignment("Jane Doe", "World History", "The Industrial Revolution");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
     }
 }
